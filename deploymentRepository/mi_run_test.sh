87bc479590f5b7ab47758ed5007be83a20b50da2
Ridmi Lokumudali <ridmi@RidmiR.local>
2019-08-13 12:12:13 +0530

    Add capp

--- deploymentRepository/mi_run_test.sh
+++ deploymentRepository/mi_run_test.sh
@@ -72,7 +72,7 @@ ls
 docker build -t mi_docker:latest -f Dockerfile .
 
 docker container ls -a
-docker container rm wso2-mi-container
+#docker container rm wso2-mi-container
 echo "Run Image ~~~~~~~~~"
 docker run -d -p 8290:8290 --name=wso2-mi-container mi_docker:latest
 
